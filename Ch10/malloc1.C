8ad88d30898606a2b026eb4007d40d0e0c29f306
fpikus <39199330+fpikus@users.noreply.github.com>
2018-05-21 05:24:44 +0800

    Create malloc1.C

---
+++ Ch10/malloc1.C
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+#include <vector>
+
+#include "benchmark/benchmark.h"
+
+void BM_malloc_free(benchmark::State& state) {
+    const size_t S = state.range(0);
+    for (auto _ : state) {
+        void* p = malloc(S);
+        benchmark::DoNotOptimize(p);
+        free(p);
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+
+//BENCHMARK(BM_malloc_free)->Arg(64);
+BENCHMARK(BM_malloc_free)->RangeMultiplier(2)->Range(32, 256);
+
+BENCHMARK_MAIN();
